6e39182a572f6fa7c071ed7cb0cdbff436ff26b2
coldlocust <lenghuai97@gmail.com>
2022-03-01 11:59:05 +0800

    测试git

---
+++ JavaScript/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: mazhao
+ * @Date: 2022-03-01 11:56:49
+ * @LastEditors: mazhao
+ * @LastEditTime: 2022-03-01 11:56:52
+ */
+print("git@github.com:coldlocust/learn-front.git")
